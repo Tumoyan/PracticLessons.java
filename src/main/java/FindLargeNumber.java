2c2c781212545c3b4a8db4fc4399346716dd3e7a
Narek Tumoyan <ntumo@bk.ru>
2022-01-15 16:40:31 +0400

    Find large number task

--- src/main/java/FindLargeNumber.java
+++ src/main/java/FindLargeNumber.java
@@ -1,6 +1,23 @@
+import java.util.Scanner;
+
 public class FindLargeNumber {
     public static void main(String[] args) {
-        int [] rand = {10,20,30,77,5};
-
+        int[] rand = {7, 20, 30, 75, 5};
+        int maxNum = rand[0];
+        /* 10 մեծ ա՞ 10 ից (false)
+           i = 20 մեծ ա՞ 10 (maxNum) ից (true) վերագրի իրան 20։ Այսինքն maxNum 10 եր փողվում ա 20
+           i = 20 մեծ ա՞ 30 (maxNum) ից (false) վերագրի իրան 30։ Այսինքն maxNum 20 եր փողվում ա 30
+           i = 30 մեծ ա՞ 77 (maxNum) ից (false) վերագրի իրան 77։ Այսինքն maxNum 30 եր փողվում ա 77
+           i = 77 մեծ ա՞ 5 (maxNum) ից (true) ուրեմն տպի արդյունքը 77։
+        */
+        for (int i : rand) {
+            if (i > maxNum)
+                maxNum = i;
+        }
+        for (int i = 0; i < rand.length; i++) {
+            System.out.print(rand[i] + " ");
+        }
+        System.out.println();
+        System.out.print("Maximum number = " + maxNum);
     }
 }
